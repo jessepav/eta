57f98f381119665b8d33ceac4d3edd456cfba53c
Ben Gubler <me@bengubler.com>
2025-11-12 14:09:49 -0700

    chore: format

--- src/config.ts
+++ src/config.ts
@@ -93,7 +93,7 @@ const defaultConfig: EtaConfig = {
   escapeFunction: XMLEscape,
   // default filter function (not used unless enables) just stringifies the input
   filterFunction: (val) => String(val),
-  outputFunctionName: 'output',
+  outputFunctionName: "output",
   functionHeader: "",
   parse: {
     exec: "",
